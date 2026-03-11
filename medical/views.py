d95784c4c4f951413bb93f263f2008d83c43ba6e
Janse-1 <chaplezjecl03@gmail.com>
2026-03-11 12:51:11 -0500

    More professional dashboard, search for patients by name or ID

--- medical/views.py
+++ medical/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .models import Paciente
-from .forms import PacienteForm
+from .forms import PacienteForm 
+from datetime import date
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+
+    query = request.GET.get('q')
+
+    pacientes = Paciente.objects.all()
+
+    if query:
+        pacientes = pacientes.filter(
+            Q(nombre__icontains=query) |
+            Q(identificacion__icontains=query)
+        )
+
+    pacientes = pacientes.order_by('-fecha_creacion')[:10]
+
+    total_pacientes = Paciente.objects.count()
+
+    context = {
+        'pacientes': pacientes,
+        'total_pacientes': total_pacientes,
+        'consultas_hoy': 0,
+        'total_expedientes': total_pacientes
+    }
+
+    return render(request,'dashboard.html',context)
 
 def lista_pacientes(request):
     pacientes = Paciente.objects.all().order_by('-fecha_creacion')
